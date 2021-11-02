2669a1160ec59d9af77b28aea4ab5af0e4a3a01a
The Loraxxz <tilmann.lorenz@mail.com>
2021-11-02 12:04:41 +0100

    schauen ob alles funktioniert

--- php/DBService.php
+++ php/DBService.php
@@ -1,9 +1,11 @@
 <?php
 
 class DBService {
-    private $host = "localhost/phpmyadmin/";
+    private $host = "127.0.0.1";
+    private $username = "root";
+    private $password = "";
     function __construct() {
-
+        $conn = new mysqli($host,$username,$password);
     }
 
 }
