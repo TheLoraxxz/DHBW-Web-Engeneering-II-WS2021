d433d15a78e112bebb9ea7718901d18466ec39bc
The Loraxxz <tilmann.lorenz@mail.com>
2021-11-12 14:26:41 +0100

    added Bootstrap over wifi

--- php/templates/Page.php
+++ php/templates/Page.php
@@ -20,8 +20,8 @@ class Page {
         echo("<!DOCTYPE html><html>");
         echo(" <head>
         <meta lang='de'>
-        <link rel='stylesheet' href='./../../css/libary/bootstrap-3.4.1-dist/css/bootstrap-theme.css'>
-        
+        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\" integrity=\"sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu\" crossorigin=\"anonymous\">
+        <script src=\"https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js\" integrity=\"sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd\" crossorigin=\"anonymous\"></script>
         <title>".$this->title."</title>");
         echo("</head>");
         echo("<body></body>");
