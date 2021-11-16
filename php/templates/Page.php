76bc3d73ce23a9db8da4facb1c7f69a4b89ccb9f
The Loraxxz <tilmann.lorenz@mail.com>
2021-11-16 12:13:45 +0100

    added login.php for login and setting cookies
    Page.php:
    - made Login Page prettier

--- php/templates/Page.php
+++ php/templates/Page.php
@@ -41,11 +41,15 @@ class Page {
     public function printPage() {
         $sessions = $this->db->getUserSession();
         $session_set = false;
-        echo("<!DOCTYPE html><html>");
-        echo(' <head>
-        <meta lang="de">
-        <link rel="stylesheet" href="'.$this->ROOTLIB  .'css/bootstrap/bootstrap.css">
-        <title>'.$this->title.'</title>');
+        echo('<!DOCTYPE html>
+              <html>');
+        echo(' 
+        <head>
+            <meta charset="UTF-8" lang="de">
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+            <link rel="stylesheet" href="'.$this->ROOTLIB  .'css/bootstrap/bootstrap.css">
+            <title>'.$this->title.'</title>
+        ');
         foreach ($this->js as $js) {
             echo('<script src="'.$js.'"></script>');
         }
@@ -65,18 +69,18 @@ class Page {
 
     private function printLogin() {
         $html = '
-            <div class="mb-3">
-                <form>
+            <div class="container" >
+                <form action="./login.php" method="post">
                     <h1>Bitte einloggen</h1>
                     <div>
                         <label for="name" class="form-label" >Name oder ID</label>
                         <input id="name" placeholder="Max Mustermann" type="text" maxlength="100" class="form-control">
                     </div>
-                    <div>
-                        <span>Passwort</span>
-                        <input type="password">
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Passwort</label>
+                        <input id="password" type="password" class="form-control">
                     </div>
-                    <button>Login</button>
+                    <button class="btn btn-primary">Login</button>
                 </form>
             </div>
         ';
