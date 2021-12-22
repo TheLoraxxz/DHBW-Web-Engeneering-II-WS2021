af4b9a5e4c0a2f88d382d6f45d0ce249a2425b34
The Loraxxz <tilmann.lorenz@mail.com>
2021-12-22 13:51:59 +0100

    weitere funktionen für die tabelle ,noch nicht nehmen noch nicht komplett fertig aber mab kann schon verschieben

--- js/forAll.js
+++ js/forAll.js
@@ -17,34 +17,46 @@ function sortTable(headerValue) {
     var img_src = headerValue.children[1].getAttribute("src");
     img_src = img_src.substr(0,img_src.indexOf("sort-"));
 
+    sorts.forEach((value)=>{
+        value["order"]++;
+    })
     var sortsindex = sorts.map((res)=>{return res.index}).indexOf(index)
     if (sortsindex===-1) {
-        sorts.push({"index":index,"desc":1,"order":1})
+        sorts.push({"index":index,"desc":1,"order":0})
+        img_src =img_src+"sort-down.png";
     } else {
-
+        sorts[sortsindex]["order"]=0;
         switch (sorts[sortsindex]["desc"]) {
             case 0:
                 sorts[sortsindex]["desc"] = 1;
+                img_src =img_src+"sort-down.png";
+                break;
             case 1:
                 sorts[sortsindex]["desc"]=-1;
+                img_src =img_src+"sort-up.png";
+                break;
             case -1:
                 sorts[sortsindex]["desc"]=0;
+                img_src =img_src+"sort-no.png";
+                break;
         }
     }
+    sorts.sort((a,b)=>{
+        if (a["order"]<b["order"]) {
+            return -1;
+        } else  if (a["order"]>b["order"]){
+            return 1;
+        }
+        return 0
+    })
+    i=0
+    sorts.forEach((sort)=>{
+        sort["order"] =i;
+        ++i;
+        headerValue.parentElement.children[sort["index"]].children[2].innerHTML=sort["order"];
+    });
 
-    switch (desc) {
-        case 0:
-            desc = 1;
-            img_src =img_src+"sort-down.png";
-            break;
-        case 1:
-            desc = -1;
-            img_src =img_src+"sort-up.png";
-            break;
-        case -1:
-            desc = 0;
-            img_src =img_src+"sort-no.png";
-    }
+
     headerValue.children[1].setAttribute("src",img_src)
     //gets the tbody and sorts it
     var tbody = document.getElementById("tableBodyTemplate");
@@ -54,38 +66,39 @@ function sortTable(headerValue) {
     tbodyKeys.forEach((key)=>{
         rowArray.push(rows[key])
     })
-    rowArray.sort((a, b)=>compareColumns(a,b,index))
+    rowArray.sort((a, b)=>compareColumns(a,b))
     tbodyKeys.forEach((key)=> {
         document.getElementById("tableBodyTemplate").appendChild(rowArray[key]);
     })
 }
 
 //function sort
-
-function compareColumns(a,b,index) {
-    if (desc===1) {
-        if (a.children[index].innerHTML < b.children[index].innerHTML) {
-            return -1
-        }
-        if (a.children[index].innerHTML > b.children[index].innerHTML) {
-            return 1
-        }
-        return 0
-    } else if(desc===-1) {
-        if (a.children[index].innerHTML < b.children[index].innerHTML) {
-            return 1
-        }
-        if (a.children[index].innerHTML > b.children[index].innerHTML) {
-            return -1
-        }
-        return 0
-    } else {
-        if(a.getAttribute("id")<b.getAttribute("id")) {
-            return -1
-        } else  if (a.getAttribute("id")>b.getAttribute("id")){
-            return 1
+/**
+ * shows which to compare --> compares the first
+ * */
+function compareColumns(a,b) {
+    for (var i =0;i<sorts.length;++i) {
+        if (sorts[0]["desc"]===1) {
+            if (a.children[sorts[i]["index"]].innerHTML < b.children[sorts[i]["index"]].innerHTML) {
+                return -1
+            }
+            if (a.children[sorts[i]["index"]].innerHTML > b.children[sorts[i]["index"]].innerHTML) {
+                return 1
+            }
+        } else if(sorts[i]["desc"]===-1) {
+            if (a.children[sorts[i]["index"]].innerHTML < b.children[sorts[i]["index"]].innerHTML) {
+                return 1
+            }
+            if (a.children[sorts[i]["index"]].innerHTML > b.children[sorts[i]["index"]].innerHTML) {
+                return -1
+            }
         } else {
-            return 0
+            if(a.getAttribute("id")<b.getAttribute("id")) {
+                return -1
+            } else  if (a.getAttribute("id")>b.getAttribute("id")){
+                return 1
+            }
         }
     }
+    return 0;
 }
